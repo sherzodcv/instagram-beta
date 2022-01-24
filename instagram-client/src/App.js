3c800631195241c84b7f5e461590653f3d687535
sherzodcv <sherzodooff@gmail.com>
2022-01-24 09:45:18 +0500

    Commit the first time for Instagram beta

---
+++ instagram-client/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+    return (
+        <div className="App">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad distinctio facere fuga in laudantium neque
+            nulla, quasi. At blanditiis ducimus est eum necessitatibus quaerat quis quod rem voluptate. Magnam, nisi.
+        </div>
+    );
+}
+
+export default App;
---
+++ instagram-server/src/main/java/my/com/instagramserver/InstagramServerApplication.java
@@ -0,0 +1,13 @@
+package my.com.instagramserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstagramServerApplication.class, args);
+    }
+
+}
