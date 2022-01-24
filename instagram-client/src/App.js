fea32d3c47ece80f796b2ce65d8870106faa66ce
sherzodcv <sherzodooff@gmail.com>
2022-01-24 10:22:53 +0500

    Commit the first time for Instagram beta

--- instagram-client/src/App.js
+++ instagram-client/src/App.js
@@ -1,6 +1,6 @@
 function App() {
     return (
-        <div className="App">
+        <div className="app">
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad distinctio facere fuga in laudantium neque
             nulla, quasi. At blanditiis ducimus est eum necessitatibus quaerat quis quod rem voluptate. Magnam, nisi.
         </div>
